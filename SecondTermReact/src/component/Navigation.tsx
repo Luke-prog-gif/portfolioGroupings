b5358becf32663cc12be9748daaca3ff55b65300
ivory19 <jelaineivorym@gmail.com>
2026-09-16 21:13:38 +0800

    fixed some section links

--- SecondTermReact/src/component/Navigation.tsx
+++ SecondTermReact/src/component/Navigation.tsx
@@ -10,7 +10,7 @@ function Navigation() {
         </a>
 
         <div className="nav-links">
-          <a href="#overview">Overview</a>
+          <a href="#header">Overview</a>
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
